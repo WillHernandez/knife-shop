6ddbe34554f48b4684a2d3916153cbe574da7d3b
Will Hernandez <w.hernandez.code@gmail.com>
2022-12-29 11:39:24 -0500

    added redirects file to public.. react router fix for netlify

--- src/components/appbar.js
+++ src/components/appbar.js
@@ -69,7 +69,7 @@ export default function ProminentAppBar() {
   return (
     <div>
       <div className="above">
-        {userEmail ? <button onClick={logoutHandler} className='logoutBtn'>Logout</button> : <a href="/login">Login/Register</a>} | <button className="helpBtn">Help</button> | <button className="contactBtn">Contact Us</button>
+        {userEmail ? <button onClick={logoutHandler} className='logoutBtn'>Logout</button> : <a href="/login"><button className='loginBtn'>Login/Register</button></a>} | <button className="helpBtn">Help</button> | <button className="contactBtn">Contact Us</button>
       </div>
 
       <Box sx={{ flexGrow: 1 }}>
